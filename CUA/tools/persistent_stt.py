62e23f6e1e28712bd44ef2c5a6aac0657632e659
Victor Manuel Martinez Garcia <victor.martinez@itcl.es>
2025-05-28 13:45:28 +0200

    Added 'hit' checker for STT

--- CUA/tools/persistent_stt.py
+++ CUA/tools/persistent_stt.py
@@ -8,23 +8,21 @@ from CUA.util.logger import logger  # noqa: F401
 from CUA.util.path import project_root_path
 
 
-
 whisper = WhisperASR()
 
-class ContinuousRecorder():
+
+class ContinuousRecorder:
     def __init__(self):
-        self.CHUNK = 1300
+        self.CHUNK = 441
         self.RATE = 4410
-        self.RECORD_SECONDS=1
+        self.RECORD_SECONDS = 1
         self.FORMAT = pyaudio.paInt16
-        self.CHANNELS = 2
-        self.SILENCE_THRESHOLD = 60
-        self.SILENCE_CHUNKS = 2
+        self.CHANNELS = 1
+        self.SILENCE_THRESHOLD = 45
         self.root = project_root_path() / "CUA/tools/output.wav"
         self.result_inference = ""
-        
 
-    def _is_silent(self,audio_data):
+    def _is_silent(self, audio_data):
         """Detect if chunk is silent based on RMS
 
         Args:
@@ -32,15 +30,18 @@ class ContinuousRecorder():
 
         Returns:
             bool: returns True if silence, False if sound.
-        """        
-        #RMS algorithm = audio_data ^ 2 -> average of audio data -> root of average -> RMS
+        """
+        # RMS algorithm = audio_data ^ 2 -> average of audio data -> root of average -> RMS
         rms = np.sqrt(np.mean(np.square(np.frombuffer(audio_data, dtype=np.int16))))
         # Uncomment loggers in case of needed otherwhise mantain this line commented as it generates a lot of logs
-        # logger.debug(f"RMS from 0.33 seconds of audio: {rms}") 
+        # logger.debug(f"RMS from 0.1 seconds of audio: {rms}")
         # logger.debug(f"Result of comparation between silence threshhold and rms : {rms < self.SILENCE_THRESHOLD}")
+        if np.isnan(rms):
+            return True
+
         return rms < self.SILENCE_THRESHOLD
-    
-    def _send_audio_endpoint(self,audio_bytes):
+
+    def _send_audio_endpoint(self, audio_bytes):
         """Sends audio to whisper endpoint
 
         Args:
@@ -48,13 +49,13 @@ class ContinuousRecorder():
 
         Returns:
             response(str): whisper inference speech to text
-        """        
+        """
         return whisper._request_asr_inference(audio_bytes)
 
     def permanent_stt(self):
-        """Permanent speech to text function, after detecting audio sound (assuming it's a voice) generates chunks of audio and composes it on a .wav to be send to whisper endpoint.
-        """        
+        """Permanent speech to text function, after detecting audio sound (assuming it's a voice) generates chunks of audio and composes it on a .wav to be send to whisper endpoint."""
         silent_count = 0
+        hit_count = 0
         recording_status = False
         p = pyaudio.PyAudio()
         recording_buffer = []
@@ -69,7 +70,6 @@ class ContinuousRecorder():
         )
 
         while True:
-
             chunk = stream.read(self.CHUNK)
             silent = self._is_silent(chunk)
 
@@ -79,47 +79,54 @@ class ContinuousRecorder():
                     silence_buffer.pop(0)
 
                 if not silent:
-                    recording_status = True
-                    recording_buffer = deepcopy(silence_buffer)
-                    silence_buffer.clear()
+                    hit_count += 1
+                    logger.warning(f"Hits:{hit_count}")
+                    if hit_count >= 4:
+                        recording_status = True
+                        recording_buffer = deepcopy(silence_buffer)
+                        silence_buffer.clear()
+
+                else:
+                    hit_count = 0
 
             else:
                 recording_buffer.append(chunk)
                 if silent:
-                    silent_count +=1
+                    silent_count += 1
                 else:
                     silent_count = 0
 
-                if silent_count >= 4:
+                if silent_count >= 5:
                     recording_status = False
                     logger.info("Sending audio to transcript...")
-                    response = self._format_chunks_and_send(self.root,recording_buffer)
+                    response = self._format_chunks_and_send(self.root, recording_buffer)
                     self.result_inference = response
 
                     silent_count = 0
+                    hit_count = 0
                     recording_buffer.clear()
 
-    def _format_chunks_and_send(self,path_record,chunks):
+    def _format_chunks_and_send(self, path_record, chunks):
         """Formats the chunks with relevant audio and sends it to whisper endpoint
         Args:
             path_record (Path): Path to where .wav will be saved
             chunks (audio_bytes): list of chunks with relevant audio
         Returns:
             response(str): response from whispers inference
-        """        
+        """
 
         p = pyaudio.PyAudio()
         with wave.open(str(path_record), "wb") as wf:
             wf.setnchannels(self.CHANNELS)
             wf.setsampwidth(p.get_sample_size(self.FORMAT))
             wf.setframerate(self.RATE)
-            wf.writeframes(b''.join(chunks))
+            wf.writeframes(b"".join(chunks))
         with open(path_record, "rb") as audio_file:
             audio_bytes = audio_file.read()
 
         response = self._send_audio_endpoint(audio_bytes)
 
         return response
-    
+
     def get_result(self):
         return self.result_inference
--- gui.py
+++ gui.py
@@ -5,7 +5,6 @@ import customtkinter as ctk
 from screeninfo import get_monitors
 
 
-
 from CUA.tools.class_browser_use import BrowserUse
 from CUA.tools.class_whisper import WhisperASR
 from CUA.tools.persistent_stt import ContinuousRecorder
@@ -14,16 +13,15 @@ from main_loop import Loop
 from threading import Thread
 
 
-
 Whisper = WhisperASR()
 Browser = BrowserUse()
 
-CUA_loop = Loop( Whisper, Browser)
+CUA_loop = Loop(Whisper, Browser)
 CUA_loop.select_agent_model(2)
 stt = ContinuousRecorder()
 
 
-#Global variables
+# Global variables
 tts_status = True
 last_result_stt = ""
 result_stt = ""
@@ -35,16 +33,18 @@ working_flag = False
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
-def agent_response(user_prompt:str):
+
+def agent_response(user_prompt: str):
     """Sends a prompt to the CUA and parses it's last response and thinking of the whole process.
 
     Args:
         user_prompt (str): User prompt
-    """    
+    """
+
     def task():
         global working_flag
         btn.configure(state="disabled")
-        #Deactivated
+        # Deactivated
         # record_btn.configure(state="disabled")
 
         agent_thinking.configure(state="normal")
@@ -56,10 +56,10 @@ def agent_response(user_prompt:str):
         agent_chat.insert(ctk.END, f"\n 😃 : {user_prompt}\n", "usuario")
         agent_chat.insert(ctk.END, "\n")
 
-        res = CUA_loop.run(user_prompt,True)
+        res = CUA_loop.run(user_prompt, True)
 
-        agent_thinking.delete(2.0,ctk.END)
-        agent_thinking.insert(ctk.END,"\n")
+        agent_thinking.delete(2.0, ctk.END)
+        agent_thinking.insert(ctk.END, "\n")
 
         # Agent thinking log extraction
         for msg in res["messages"]:
@@ -72,19 +72,23 @@ def agent_response(user_prompt:str):
                             tool = block["name"]
                             inputs = block["input"]
                             agent_thinking.insert(ctk.END, f" 🔧 Herramienta: {tool}\n")
-                            agent_thinking.insert(ctk.END, f" 🔢 Parámetros: {inputs}\n")
+                            agent_thinking.insert(
+                                ctk.END, f" 🔢 Parámetros: {inputs}\n"
+                            )
                 agent_thinking.insert(ctk.END, "-----------------------------\n")
 
             elif hasattr(msg, "tool_call_id"):
                 agent_thinking.insert(
-                    ctk.END, f" ✉ Respuesta de herramienta: {msg.content}\n","thinking"
+                    ctk.END, f" ✉ Respuesta de herramienta: {msg.content}\n", "thinking"
                 )
                 agent_thinking.insert(ctk.END, "-----------------------------\n")
 
-        agent_thinking.insert(ctk.END, " 🧠 mente en frío\n","thinking")
+        agent_thinking.insert(ctk.END, " 🧠 mente en frío\n", "thinking")
         agent_thinking.insert(ctk.END, "-----------------------------\n")
 
-        agent_chat.insert(ctk.END, f" 🐄 : {res['messages'][-1].content}\n", "asistente")
+        agent_chat.insert(
+            ctk.END, f" 🐄 : {res['messages'][-1].content}\n", "asistente"
+        )
 
         agent_thinking.configure(state="disabled")
         agent_chat.configure(state="disabled")
@@ -92,10 +96,10 @@ def agent_response(user_prompt:str):
         agent_thinking.see(ctk.END)
 
         if tts_status:
-            CUA_loop.text_to_speech(res['messages'][-1].content)
+            CUA_loop.text_to_speech(res["messages"][-1].content)
 
         btn.configure(state="normal")
-        #Deactivated
+        # Deactivated
         # record_btn.configure(state="normal")
 
         working_flag = False
@@ -104,8 +108,8 @@ def agent_response(user_prompt:str):
 
 
 def agent_stt():
-    """Call to whisper for obtaining the text of the invoice prompt
-    """    
+    """Call to whisper for obtaining the text of the invoice prompt"""
+
     def task():
         user_prompt = CUA_loop.get_whisper_prompt()
         agent_response(user_prompt=user_prompt)
@@ -114,15 +118,21 @@ def agent_stt():
 
 
 def clicked():
-    """ Action after clicking "Enviar" button.
-    """    
+    """Action after clicking "Enviar" button."""
     user_prompt = entry.get().strip()
     entry.delete(0, ctk.END)
     agent_response(user_prompt=user_prompt)
 
 
-def create_centered_popup(title: str, message: str, width: int = 500, height: int = 350, font: int = 30, time_alive: int = None): #type: ignore
-    #Taken idea from https://stackoverflow.com/questions/3352918/how-to-center-a-window-on-the-screen-in-tkinter
+def create_centered_popup(
+    title: str,
+    message: str,
+    width: int = 500,
+    height: int = 350,
+    font: int = 30,
+    time_alive: int = None,# type: ignore
+):  
+    # Taken idea from https://stackoverflow.com/questions/3352918/how-to-center-a-window-on-the-screen-in-tkinter
     """Creates a centered popup in root window
 
     Args:
@@ -132,7 +142,7 @@ def create_centered_popup(title: str, message: str, width: int = 500, height: in
         height (int): popup height.
         font (str): font size.
         time_alive (int, opcional): time_alive of window.
-    
+
     Returns:
         popup (Toplevel): Popup reference
     """
@@ -155,59 +165,75 @@ def create_centered_popup(title: str, message: str, width: int = 500, height: in
     popup.attributes("-topmost", True)
     popup.focus_force()
 
-    ctk.CTkLabel(popup, text=message, wraplength=width - 50, font=ctk.CTkFont(size=font),justify="left").pack()
+    ctk.CTkLabel(
+        popup,
+        text=message,
+        wraplength=width - 50,
+        font=ctk.CTkFont(size=font),
+        justify="left",
+    ).pack()
 
     if time_alive is not None:
         popup.after(time_alive, popup.destroy)
 
     return popup
 
+
 def reset_popup():
-    """Opens a popup informing the user that the program will reset in 5 seconds and procceeds to reset.
-    """    
+    """Opens a popup informing the user that the program will reset in 5 seconds and procceeds to reset."""
+
     def reset():
         os.execv(sys.executable, ["python"] + sys.argv)
-        
-    top = create_centered_popup("ABORTANDO!!","Se procedera a abortar y reiniciarse en 5 segundos.")
+
+    top = create_centered_popup(
+        "ABORTANDO!!", "Se procedera a abortar y reiniciarse en 5 segundos."
+    )
 
     top.after(5000, reset)
 
+
 def accept_prompt():
-    """Generates a windows with user's prompt asking if the prompt should be accepted or not
-    """    
-    global prompt_accept_flag,prompt_popup
-    prompt_popup= create_centered_popup("¿Aceptar este prompt?",f"¿Quiere aceptar este prompt?: \n- {last_prompt}. \n\n\n Responda aceptar en caso afirmativo, denegar en caso negativo.")
-    
+    """Generates a windows with user's prompt asking if the prompt should be accepted or not"""
+    global prompt_accept_flag, prompt_popup
+    prompt_popup = create_centered_popup(
+        "¿Aceptar este prompt?",
+        f"¿Quiere aceptar este prompt?: \n- {last_prompt}. \n\n\n Responda aceptar en caso afirmativo, denegar en caso negativo.",
+    )
+
     prompt_accept_flag = True
 
+
 def safe_abort():
-    """Abort CUA actual execution, it waits for the actual execution to end to exit safely.
-    """    
-    create_centered_popup("Abortando de manera segura.","Abortando la ejecución del agente de manera segura, espere un momento.",time_alive=10000)
+    """Abort CUA actual execution, it waits for the actual execution to end to exit safely."""
+    create_centered_popup(
+        "Abortando de manera segura.",
+        "Abortando la ejecución del agente de manera segura, espere un momento.",
+        time_alive=10000,
+    )
     CUA_loop.set_stoppable(True)
-    
+
+
 def record_clicked():
-    """Records a prompt of 5 seconds and sends it to the CUA agent
-    """    
-    create_centered_popup("Grabando", "Se procedera a grabar durante 5 segundos", time_alive=5000)
+    """Records a prompt of 5 seconds and sends it to the CUA agent"""
+    create_centered_popup(
+        "Grabando", "Se procedera a grabar durante 5 segundos", time_alive=5000
+    )
     agent_stt()
 
+
 def reset_click():
-    """resets the whole program instantly after waiting 5 seconds.
-    """    
+    """resets the whole program instantly after waiting 5 seconds."""
     reset_popup()
 
+
 def toggle_tts():
-    """text to speech toggle of IA response
-    """    
+    """text to speech toggle of IA response"""
     global tts_status
     tts_status = not tts_status
     toggle_btn.configure(
         text=f"TTS: {'ON 🔊' if tts_status else 'OFF 🔇'}",
-        fg_color="#28a745" if tts_status else "#6c757d"
+        fg_color="#28a745" if tts_status else "#6c757d",
     )
-
-
 
 
 # GUI tkinter
@@ -221,14 +247,13 @@ for m in get_monitors():
     if m.is_primary:
         window_size = m.width
 
-root.geometry(f"+{window_size}+0")  
+root.geometry(f"+{window_size}+0")
 root.lift()
 root.attributes("-topmost", True)
 root.after(1000, lambda: root.attributes("-topmost", False))
 
 
-root.after(500, lambda: root.state("zoomed"))  
-
+root.after(500, lambda: root.state("zoomed"))
 
 
 root.grid_columnconfigure(0, weight=1)
@@ -237,12 +262,16 @@ root.grid_rowconfigure(1, weight=1)
 entry = ctk.CTkEntry(root, width=150)
 entry.grid(row=0, column=0, padx=15, pady=10, sticky="ew", columnspan=1)
 
-agent_chat = ctk.CTkTextbox(root, wrap="word", font=("Courier New", 14),fg_color="#1e1e1e",width=350)
+agent_chat = ctk.CTkTextbox(
+    root, wrap="word", font=("Courier New", 14), fg_color="#1e1e1e", width=350
+)
 agent_chat.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 agent_chat.insert(ctk.END, "Este es el inicio de su conversación.\n")
 agent_chat.configure(state="disabled")
 
-agent_thinking = ctk.CTkTextbox(root, wrap="word", font=("Courier New", 14),fg_color="#1e1e1e")
+agent_thinking = ctk.CTkTextbox(
+    root, wrap="word", font=("Courier New", 14), fg_color="#1e1e1e"
+)
 agent_thinking.grid(row=1, column=1, padx=5, pady=10, sticky="nsew", columnspan=5)
 agent_thinking.insert(ctk.END, "Pensamientos del agente y herramientas usadas:\n\n")
 agent_thinking.configure(state="disabled")
@@ -250,7 +279,7 @@ agent_thinking.configure(state="disabled")
 agent_chat.tag_config("usuario", foreground="#F87070")
 agent_chat.tag_config("asistente", foreground="#8BE9FD")
 
-agent_thinking.tag_config("thinking",foreground="#C3C3C3")
+agent_thinking.tag_config("thinking", foreground="#C3C3C3")
 
 
 control_frame = ctk.CTkFrame(root, fg_color="transparent")
@@ -258,31 +287,60 @@ control_frame.grid(row=0, column=1, columnspan=4, padx=10, pady=(10, 5), sticky=
 
 btn_width = 150
 
-btn = ctk.CTkButton(control_frame, text="📤 Enviar", fg_color="#007ACC", width=btn_width, command=clicked)
+btn = ctk.CTkButton(
+    control_frame,
+    text="📤 Enviar",
+    fg_color="#007ACC",
+    width=btn_width,
+    command=clicked,
+)
 btn.pack(side="left", padx=5, pady=5)
 
-#Deactivated as persistent STT is implemented, might be changed to something else
+# Deactivated as persistent STT is implemented, might be changed to something else
 # record_btn = ctk.CTkButton(control_frame, text="🎤 Voz", fg_color="#28a745", width=btn_width, command=record_clicked)
 # record_btn.pack(side="left", padx=5, pady=5)
 
-abort_btn = ctk.CTkButton(control_frame, text="⛔ Abortar", fg_color="#dc3545", width=btn_width, command=safe_abort)
+abort_btn = ctk.CTkButton(
+    control_frame,
+    text="⛔ Abortar",
+    fg_color="#dc3545",
+    width=btn_width,
+    command=safe_abort,
+)
 abort_btn.pack(side="left", padx=5, pady=5)
 
-reset_btn = ctk.CTkButton(control_frame, text="🔄 Reiniciar", fg_color="#5116dd", width=btn_width, command=reset_click)
+reset_btn = ctk.CTkButton(
+    control_frame,
+    text="🔄 Reiniciar",
+    fg_color="#5116dd",
+    width=btn_width,
+    command=reset_click,
+)
 reset_btn.pack(side="left", padx=5, pady=5)
 
-toggle_btn = ctk.CTkButton(control_frame, text=" TTS: ON 🔊", fg_color="#28a745", width=btn_width, command=toggle_tts)
+toggle_btn = ctk.CTkButton(
+    control_frame,
+    text=" TTS: ON 🔊",
+    fg_color="#28a745",
+    width=btn_width,
+    command=toggle_tts,
+)
 toggle_btn.pack(side="left", padx=5, pady=5)
 
 
 def stt_thread():
-    """Thread with the STT logic
-    """    
+    """Thread with the STT logic"""
+
     def monitor_stt():
         Thread(target=stt.permanent_stt, daemon=True).start()
 
         def check_for_updates():
-            global last_result_stt,prompt_accept_flag,last_prompt,prompt_popup,working_flag
+            global \
+                last_result_stt, \
+                prompt_accept_flag, \
+                last_prompt, \
+                prompt_popup, \
+                working_flag
             result = stt.get_result()
             if result and result != last_result_stt:
                 last_result_stt = result
@@ -293,23 +351,25 @@ def stt_thread():
                         prompt_accept_flag = False
                         agent_response(last_prompt)
                         entry.delete(0, ctk.END)
-                        prompt_popup.destroy() #type: ignore
+                        prompt_popup.destroy()  # type: ignore
                         working_flag = True
 
                     elif "denegar" in parsed_response:
                         prompt_accept_flag = False
                         entry.delete(0, ctk.END)
-                        prompt_popup.destroy() #type: ignore
-
+                        prompt_popup.destroy()  # type: ignore
+                    else:
+                        create_centered_popup(
+                            "ASR Inference",
+                            f"Te he entendido: {result}, los respuestas posibles solo son ACEPTAR o DENEGAR.",
+                            time_alive=5000,
+                        )
 
                 elif working_flag:
                     if "reiniciar" in parsed_response:
                         reset_popup()
                     elif "abortar" in parsed_response:
                         safe_abort()
-                    # else:
-                    #     # fading_popup("ASR Inference",f"Te he entendido: {result}, los respuestas posibles solo son ACEPTAR o DENEGAR.",3000)
-                    #     create_centered_popup("ASR Inference",f"Te he entendido: {result}, los respuestas posibles solo son ACEPTAR o DENEGAR.",time_alive=5000)
 
                 else:
                     entry.delete(0, ctk.END)

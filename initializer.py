8fe3dc4c0812d92665f7eec432dbb16498343b25
Victor Manuel Martinez Garcia <victor.martinez@itcl.es>
2025-06-03 11:04:26 +0200

    added initializer of containers with LLMs on ITCL server

---
+++ initializer.py
@@ -0,0 +1,15 @@
+from main_loop import Loop
+from CUA.tools.class_whisper import WhisperASR
+from CUA.tools.class_browser_use import BrowserUse
+
+
+whisper = WhisperASR()
+browser = BrowserUse()
+
+initializer_loop_endpoints = Loop(whisper,browser)
+initializer_loop_endpoints.select_agent_model(2)
+
+def initialize_endpoints():
+    initializer_loop_endpoints.run("Utiliza tu herramienta ScreenInterpreter para decirme que ves")
+    initializer_loop_endpoints.run("Utiliza tu herramienta SimpleScreenInterpreter para decirme que ves")
+    whisper.whisper_TTS("Inicializando")
